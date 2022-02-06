f171f3fe00b43f4b4f73776669c604d43f8f164e
Rei-Suzuki1729 <eva2021end@gmail.com>
2022-02-06 22:37:09 +0900

    Created data structure

--- MiniLips/MiniLips.c
+++ MiniLips/MiniLips.c
@@ -6,19 +6,30 @@ enum tag
 	EMP,//空
 	NUM,//数
 	SYM,//シンボル
+	LIS,//リスト
+	SUBR,//組み込み関数
 };
 
 typedef tag tag;
 
+enum flag
+{
+	FRE,
+	USE
+};
 
+typedef flag flag;
 
 struct cell
 {
 	tag tag;//種類を記憶しておく場所
+	flag flag;//GC(ガページコレクション)の時に使う目印
 	char* name;//シンボルの名前を記憶する場所
 	union {
 		int num;//数アトムとして使われている場合のその数の値
-	};
+		int bind;//参照する場合の参照する番地
+		int (*subr)();//関数ポインタ
+	}val;
 	int car;//アトムが格納されているアドレス
 	int cdr;//次の番地のアドレス
 };
