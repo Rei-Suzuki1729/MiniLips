0887b1845eca0d17fa72ba6eb3fa4e6f7eba6eb5
Rei-Suzuki1729 <eva2021end@gmail.com>
2022-02-07 16:31:54 +0900

    created gettoken_function

--- MiniLips/MiniLips.c
+++ MiniLips/MiniLips.c
@@ -1,24 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
+
+#define BUFSIZE 256
 #include <stdio.h>
 
-enum tag
+typedef enum 
 {
 	EMP,//空
 	NUM,//数
 	SYM,//シンボル
 	LIS,//リスト
 	SUBR,//組み込み関数
-};
+}tag;
 
-typedef tag tag;
-
-enum flag
+typedef enum 
 {
 	FRE,
 	USE
-};
+}flag;
 
-typedef flag flag;
 
 struct cell
 {
@@ -36,6 +35,134 @@ struct cell
 
 typedef struct cell cell;
 
+//-------read--------
+#define EOL     '\n'
+#define TAB     '\t'
+#define SPACE   ' '
+#define ESCAPE  033
+#define NUL     '\0'
+
+
+
+//----read---------
+typedef enum { 
+    LPAREN,// "("
+    RPAREN,// ")"
+    QUOTE, // "\"
+    DOT,   // "."
+    NUMBER,
+    SYMBOL,
+    OTHER
+} toktype;
+
+
+typedef enum {
+    GO,
+    BACK
+} backtrack;
+
+typedef struct {
+    char ch;
+    backtrack flag;
+    toktype type;
+    char buf[BUFSIZE];
+} token;
+
+token stok = { GO,OTHER };
+
+
+void gettoken(void) {
+    char c;
+    int pos;
+
+    if (stok.flag == BACK) {
+        stok.flag = GO;
+        return;
+    }
+
+    if (stok.ch == ')') {
+        stok.type = RPAREN;
+        stok.ch = NUL;
+        return;
+    }
+
+    if (stok.ch == '(') {
+        stok.type = LPAREN;
+        stok.ch = NUL;
+        return;
+    }
+
+    c = getchar();
+    while ((c == SPACE) || (c == EOL) || (c == TAB))
+        c = getchar();
+
+    switch (c) {
+    case '(':   stok.type = LPAREN; break;
+    case ')':   stok.type = RPAREN; break;
+    case '\'':  stok.type = QUOTE; break;
+    case '.':   stok.type = DOT; break;
+    default: {
+        pos = 0; stok.buf[pos++] = c;
+        while (((c = getchar()) != EOL) && (pos < BUFSIZE) &&
+            (c != SPACE) && (c != '(') && (c != ')'))
+            stok.buf[pos++] = c;
+
+        stok.buf[pos] = NUL;
+        stok.ch = c;
+        if (numbertoken(stok.buf)) {
+            stok.type = NUMBER;
+            break;
+        }
+        if (symboltoken(stok.buf)) {
+            stok.type = SYMBOL;
+            break;
+        }
+        stok.type = OTHER;
+    }
+    }
+}
+
+int numbertoken(char buf[]) {
+    int i;
+    char c;
+
+    if ((buf[0] == '+') || (buf[0] == '-')) {
+        if (buf[1] == NUL)
+            return(0); // case {+,-} => symbol
+        i = 1;
+        while ((c = buf[i]) != NUL)
+            if (isdigit(c))
+                i++;  // case {+123..., -123...}
+            else
+                return(0);
+    }
+    else {
+        i = 0;    // {1234...}
+        while ((c = buf[i]) != NUL)
+            if (isdigit(c))
+                i++;
+            else
+                return(0);
+    }
+    return(1);
+}
+
+int symboltoken(char buf[]) {
+    int i;
+    char c;
+
+    if (isdigit(buf[0]))
+        return(0);
+
+    i = 0;
+    while ((c = buf[i]) != NUL)
+        if ((isalpha(c)) || (isdigit(c)) || (issymch(c)))
+            i++;
+        else
+            return(0);
+
+    return(1);
+}
 
 
 int main(void) {
