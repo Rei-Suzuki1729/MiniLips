30e277c3ad7d97b4c3ab8a0bef83a225931743cf
Rei-Suzuki1729 <eva2021end@gmail.com>
2022-02-13 19:06:18 +0900

    created eval for atom

--- MiniLips/MiniLips.c
+++ MiniLips/MiniLips.c
@@ -1,4 +1,5 @@
 #define HEAPSIZE 10000000
+#define STACKSIZE  30000
 #define BUFSIZE 256
 #define SYMSIZE 256
 #define NIL 0
@@ -59,9 +60,14 @@ typedef struct
 
 
 cell heap[HEAPSIZE];
-int hp;//heap pointer
-int fc; //free counter
 
+
+//------pointer----
+int ep; //environment pointer
+int hp; //heap pointer 
+int sp; //stack pointer
+int fc; //free counter
+int ap; //arglist pointer
 
 //-------read--------
 #define EOL     '\n'
@@ -99,8 +105,13 @@ token stok = { GO,OTHER };
 
 jmp_buf buf; //goto
 
-void initcell(void);
+int stack[STACKSIZE];
 
+//--------------------------
+void initcell(void);
+int freshcell(void);
+void assocsym(int sym, int val);
+int findsym(int sym);
 //---------------------
 int car(int addr);
 int cdr(int addr);
@@ -109,29 +120,54 @@ int caar(int addr);
 int cdar(int addr);
 int cadr(int addr);
 int caddr(int addr);
+int assoc(int sym, int lis);
+int length(int addr);
+int list(int addr);
 //-------------------------
 
-
+int makenum(int num);
+int makesym(char* name);
+//----------------------------
 void gettoken(void);
 int numbertoken(char buf[]);
 int symboltoken(char buf[]);
 int issymch(char c);
-
-int freshcell(void);
-int makenum(int num);
-int makesym(char* name);
-
 int read(void);
 int readlist(void);
-
+//-----------------------------
+//-------------------------------
 void print(int addr);
 void printlist(int addr);
+//-------------------------------
+
+//-------------------------------------
+int eval(int addr);
+void bindarg(int lambda, int arglist);
+void unbind(void);
+int atomp(int addr);
+int numberp(int addr);
+int symbolp(int addr);
 int listp(int addr);
 int nullp(int addr);
-
-
+int eqp(int addr1, int addr2);
+int evlis(int addr);
+int apply(int func, int arg);
+int subrp(int addr);
+void initsubr(void);
+void defsubr(char* symname, int(*func)(int));
+void bindfunc(char* name, tag tag, int(*func)(int));
+void push(int pt);
+int pop(void);
 void error(int errnum, char* fun, int arg);
-int length(int addr);
+
+
+int f_plus(int arglist);
+int f_minus(int arglist);
+int f_mult(int arglist);
+int f_div(int arglist);
+int f_exit(int arglist);
+
+
 
 
 
@@ -139,6 +175,7 @@ int length(int addr);
 int main(void) {
     printf("MiniLis Ver0.01\n");
     initcell();
+    initsubr();
     int ret = setjmp(buf);
 
 repl:
@@ -146,7 +183,7 @@ repl:
         while (1)
         {
             printf("> "); fflush(stdout); fflush(stdin);
-            print(read());
+            print(eval(read()));
             printf("\n"); fflush(stdout);
         }
     }
@@ -164,11 +201,6 @@ repl:
 
 
 
-
-
-
-
-
 //初期化・自由化リスト
 void initcell(void) {
     int addr;
@@ -180,8 +212,159 @@ void initcell(void) {
 
     hp = 0;
     fc = HEAPSIZE;
+
+    //0番地はnil、環境レジスタを設定する。初期環境
+    ep = makesym("nil");
+    assocsym(makesym("nil"), NIL);
+    assocsym(makesym("t"), makesym("t"));//ep = ((t.t)(nil.nil))という連想リスト
+
+    sp = 0;
+    ap = 0;
 }
 
+int freshcell(void) {
+    int res;
+
+    res = hp;
+    hp = heap[hp].cdr;
+    heap[res].cdr = 0;
+    fc--;
+    return(res);
+}
+
+//deep-bindによる。シンボルが見つからなかったら登録。
+//見つかったらそこに値をいれておく。
+void bindsym(int sym, int val) {
+    int addr;
+
+    addr = assoc(sym, ep);
+    if (addr == 0)
+        assocsym(sym, val);
+    else
+        heap[addr].cdr = val;
+}
+
+
+//変数の束縛
+//ローカル変数の場合は以前の束縛に積み上げていく。
+//したがって同じ変数名があったとしても書き変えない。
+void assocsym(int sym, int val) {
+    ep = cons(cons(sym, val), ep);
+}
+
+
+//環境は次のように連想リストになっている。
+// env = ((sym1 . val1) (sym2 . val2) ...)
+// assocでシンボルに対応する値を探す。
+//見つからなかった場合には-1を返す。
+int findsym(int sym) {
+    int addr;
+
+    addr = assoc(sym, ep);
+
+    if (addr == 0)
+        return(-1);
+    else
+        return(cdr(addr));
+}
+
+
+//-----------------------
+
+//---------------------リスト操作-------------------------
+int car(int addr) {
+    return(heap[addr].car);
+}
+
+int caar(int addr) {
+    return(car(car(addr)));
+}
+
+int cdar(int addr) {
+    return(cdr(car(addr)));
+}
+
+int cdr(int addr) {
+    return(heap[addr].cdr);
+}
+
+int cadr(int addr) {
+    return(car(cdr(addr)));
+}
+
+int caddr(int addr) {
+    return(car(cdr(cdr(addr))));
+}
+
+//アトムをくっつける関数
+int cons(int car, int cdr) {
+    int addr;
+
+    addr = freshcell();
+    heap[addr].tag = LIS;
+    heap[addr].car = car;
+    heap[addr].cdr = cdr;
+    return(addr);
+}
+
+int assoc(int sym, int lis) {
+    if (nullp(lis))
+        return(0);
+    else if (eqp(sym, caar(lis)))
+        return(car(lis));
+    else
+        return(assoc(sym, cdr(lis)));
+}
+
+int length(int addr) {
+    int len = 0;
+
+    while (!(nullp(addr))) {
+        len++;
+        addr = cdr(addr);
+    }
+    return(len);
+}
+
+int list(int arglist) {
+    if (nullp(arglist))
+        return(NIL);
+    else
+        return(cons(car(arglist), list(cdr(arglist))));
+}
+//--------------------------------------------
+
+
+//数アトムを生成
+int makenum(int num) {
+    int addr;
+
+    addr = freshcell();
+    heap[addr].tag = NUM;
+    heap[addr].val.num = num;
+    return(addr);
+}
+
+//シンボルアトムの生成
+int makesym(char* name) {
+    int addr;
+
+    addr = freshcell();
+    heap[addr].tag = SYM;
+    heap[addr].name = (char*)malloc(SYMSIZE); 
+    strcpy(heap[addr].name,name);
+    return(addr);
+}
+
+//スタック。ep環境ポインタの保存用
+void push(int pt) {
+    stack[sp++] = pt;
+}
+int pop(void) {
+    return(stack[--sp]);
+}
+
+//------------read-----------------------------------------
 
 //--------スキャナー---------------------
 void gettoken(void) {
@@ -240,7 +423,7 @@ int numbertoken(char buf[]) {
     char c;
 
     if ((buf[0] == '+') || (buf[0] == '-')) {
-        if (buf[1] == NUL)
+        if (buf[1] == NUL) 
             return(0); // case {+,-} => symbol
         i = 1;
         while ((c = buf[i]) != NUL)
@@ -291,94 +474,7 @@ int issymch(char c) {
     default:  return(0);
     }
 }
-
-
-
-//-----------------------
-
-int freshcell(void) {
-    int res;
-
-    res = hp;
-    hp = heap[hp].cdr;
-    heap[res].cdr = 0;
-    fc--;
-    return(res);
-}
-
-//数アトムを生成
-int makenum(int num) {
-    int addr;
-
-    addr = freshcell();
-    heap[addr].tag = NUM;
-    heap[addr].val.num = num;
-    return(addr);
-}
-
-//シンボルアトムの生成
-int makesym(char* name) {
-    int addr;
-
-    addr = freshcell();
-    heap[addr].tag = SYM;
-    heap[addr].name = (char*)malloc(SYMSIZE); 
-    strcpy(heap[addr].name,name);
-    return(addr);
-}
-
-//リスト操作
-int car(int addr) {
-    return(heap[addr].car);
-}
-
-int caar(int addr) {
-    return(car(car(addr)));
-}
-
-int cdar(int addr) {
-    return(cdr(car(addr)));
-}
-
-int cdr(int addr) {
-    return(heap[addr].cdr);
-}
-
-int cadr(int addr) {
-    return(car(cdr(addr)));
-}
-
-int caddr(int addr) {
-    return(car(cdr(cdr(addr))));
-}
-
-
-
-//アトムをくっつける関数
-int cons(int car, int cdr) {
-    int addr;
-
-    addr = freshcell();
-    heap[addr].tag = LIS;
-    heap[addr].car = car;
-    heap[addr].cdr = cdr;
-    return(addr);
-}
-
-int length(int addr) {
-    int len = 0;
-
-    while (!(nullp(addr))) {
-        len++;
-        addr = cdr(addr);
-    }
-    return(len);
-}
-
-
-
-//-------------------------------
-
+//--------------------
 int read(void) {
     gettoken();
     switch (stok.type) {
@@ -412,9 +508,9 @@ int readlist(void) {
             return(cons(car, cdr));
         }
 }
+//-----------------------------------------------------------
 
-
-//-----print------------------
+//-----print------------------------------------------------
 void print(int addr) {
     switch (heap[addr].tag) {
     case NUM:   printf("%d", heap[addr].val.num); break;
@@ -425,8 +521,6 @@ void print(int addr) {
     default:    printf("<undef>"); break;
     }
 }
-
-
 
 void printlist(int addr) {
     if (addr == 0 || addr == 1)
@@ -446,43 +540,136 @@ void printlist(int addr) {
             printlist(heap[addr].cdr);
         }
 }
+//----------------------------------------------------------
+//-----------eval-------------------------------------------
 
+int eval(int addr) {
+    int res;
+    //アトムの場合
+    if (atomp(addr)) {
+        //数の場合
+        if (numberp(addr))
+            return(addr);
+        //シンボルの場合
+        if (symbolp(addr)) {
+            res = findsym(addr);
+            if (res == -1)
+                error(CANT_FIND_ERR, "eval", addr);
+            else
+                return(res);
+        }
+    }
+    else//リストの場合
+        if (listp(addr)) {
+            if ((symbolp(car(addr))) && (strcmp(heap[car(addr)].name,"quote") == 0))
+                return(cadr(addr));
+            if (numberp(car(addr)))
+                error(ARG_SYM_ERR, "eval", addr);
+            if (subrp(car(addr)))
+                return(apply(car(addr), evlis(cdr(addr))));
+        }
+    error(CANT_FIND_ERR, "eval", addr);
+    return(0);
+}
+
+int apply(int func, int args) {
+    int symaddr;
+
+    symaddr = findsym(func); //組み込み関数の場合はdefsubrで定義している
+    if (symaddr == -1)
+        error(CANT_FIND_ERR, "apply", func);
+    else {
+        switch (heap[symaddr].tag) {
+        case SUBR:  return((heap[symaddr].val.subr)(args));
+        default:    error(ILLEGAL_OBJ_ERR, "eval", symaddr);
+        }
+    }
+    return(0);
+}
+
+void bindarg(int varlist, int arglist) {
+    int arg1, arg2;
+
+    push(ep);
+    while (!(varlist == 0 || varlist == 1)) {
+        arg1 = car(varlist);
+        arg2 = car(arglist);
+        assocsym(arg1, arg2);
+        varlist = cdr(varlist);
+        arglist = cdr(arglist);
+    }
+}
+
+void unbind(void) {
+    ep = pop();
+}
+
+//変数を束縛されている値に置き換える
+int evlis(int addr) {
+    if (addr == 0 ||addr == 1) {
+        return(addr);
+    }
+    else {
+        return(cons(eval(car(addr)),evlis(cdr(addr))));
+    }
+}
+
+//アトムかどうかタグで判定
 int atomp(int addr) {
     if (heap[addr].tag == NUM || heap[addr].tag == SYM)
         return(1);
     else
         return(0);
 }
-
+//数かどうかタグで判定
 int numberp(int addr) {
     if (heap[addr].tag == NUM)
         return(1);
     else
         return(0);
 }
-
+//シンボルかどうかタグで判定
 int symbolp(int addr) {
     if (heap[addr].tag == SYM)
         return(1);
     else
         return(0);
 }
-//
+//リストかどうかタグで判定
 int listp(int addr) {
     if (heap[addr].tag == LIS || addr == 0 || addr == 1)
         return(1);
     else
         return(0);
 }
-
-//
+//番地が0か1の判定場合
 int nullp(int addr) {
     if (addr == 0 || addr == 1)
         return(1);
     else
         return(0);
 }
+//同じかどうか判定
+int eqp(int addr1, int addr2) {
+    if ((numberp(addr1)) && (numberp(addr2))
+        && ((heap[addr1].val.num) == (heap[addr2].val.num)))
+        return(1);
+    else if ((symbolp(addr1)) && (symbolp(addr2))
+        && (strcmp(heap[addr1].name,heap[addr2].name) == 0))
+        return(1);
+    else
+        return(0);
+}
+//組み込み関数かどうかタグで判定
+int subrp(int addr) {
+    int val;
 
+    val = findsym(addr);
+    if (val != -1)
+        return(heap[addr].tag == SUBR);
+    else
+        return(0);
+}
 
 //-------エラー処理------
 void error(int errnum, char* fun, int arg) {
@@ -525,4 +712,99 @@ void error(int errnum, char* fun, int arg) {
     }
     printf("\n");
     longjmp(buf, 1);
+}
+
+
+
+//--------組込み関数---------------------------------
+//subrを環境に登録する。
+void defsubr(char* symname, int(*func)(int)) {
+    bindfunc(symname, SUBR, func);
+}
+
+void bindfunc(char* name, tag tag, int(*func)(int)) {
+    int sym, val;
+
+    sym = makesym(name);
+    val = freshcell();
+    heap[val].tag = tag;
+    heap[val].val.subr = func;
+    heap[val].cdr = 0;
+    bindsym(sym, val);
+}
+
+void initsubr(void) {
+    defsubr("+", f_plus);
+    defsubr("-", f_minus);
+    defsubr("*", f_mult);
+    defsubr("/", f_div);
+    defsubr("exit", f_exit);
+    defsubr("quit", f_exit);
+}
+   
+
+//-----組み込み関数定義---------------------
+
+int f_plus(int arglist) {
+    int arg, res;
+
+    res = 0;
+    while (!(arglist == 0 || arglist == 1)) {
+        arg = heap[car(arglist)].val.num;
+        arglist = cdr(arglist);
+        res = res + arg;
+    }
+    return(makenum(res));
+}
+
+int f_minus(int arglist) {
+    int arg, res;
+
+    res = heap[car(arglist)].val.num;
+    arglist = cdr(arglist);
+    while (!(arglist == 0 || arglist == 1)) {
+        arg = heap[car(arglist)].val.num;
+        arglist = cdr(arglist);
+        res = res - arg;
+    }
+    return(makenum(res));
+}
+
+int f_mult(int arglist) {
+    int arg, res;
+
+    res = heap[car(arglist)].val.num;
+    arglist = cdr(arglist);
+    while (!(arglist == 0 || arglist == 1)) {
+        arg = heap[car(arglist)].val.num;
+        arglist = cdr(arglist);
+        res = res * arg;
+    }
+    return(makenum(res));
+}
+
+int f_div(int arglist) {
+    int arg, res;
+
+    res = heap[car(arglist)].val.num;
+    arglist = cdr(arglist);
+    while (!(arglist == 0 || arglist == 1)) {
+        arg = heap[car(arglist)].val.num;
+        if (arg == 0)
+            error(DIV_BY_ZERO, "/", arglist);
+        arglist = cdr(arglist);
+        res = res / arg;
+    }
+    return(makenum(res));
+}
+
+
+int f_exit(int arglist) {
+    int addr;
+
+    for (addr = 0; addr < HEAPSIZE; addr++)
+        free(heap[addr].name);
+
+    printf("- good bye -\n");
+    longjmp(buf, 2);
 }
